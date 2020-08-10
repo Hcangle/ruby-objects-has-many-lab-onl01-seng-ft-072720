05b31044dcf2bd0fcd12cca361a53fee312a83d8
Hannah Angle <hannahcangle@gmail.com>
2020-08-10 20:14:10 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,10 +14,6 @@ class Author
     @@post_count += 1
   end 
   
-  def posts
-    @posts
-  end 
-  
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     add_post(post)
@@ -27,6 +23,10 @@ class Author
   def self.post_count 
     @@post_count
   end 
+  
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
 end 
   
   
